ff0136e19200576dab266f1bf079d49ccee9b46d
mariambuzaladze <mariambuzaladze10@example.come>
2024-03-08 21:00:49 +0400

    add elements of page component and style them

--- src/components/Page/Page.jsx
+++ src/components/Page/Page.jsx
@@ -2,5 +2,16 @@ import "./page.css";
 import Notification from "../Notification/Notification";
 
 export default function Page() {
-  return <></>;
+  return (
+    <div className="content">
+      <header>
+        <div className="title">
+          <h1>Notifications</h1>
+          <div className="container">3</div>
+        </div>
+        <p className="read">Mark all as read</p>
+      </header>
+      <main></main>
+    </div>
+  );
 }
